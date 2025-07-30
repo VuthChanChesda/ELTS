57d5bb4ad02940789ad28d51a47da0ee55135836
vuth chan chesda <vuthchanchesda@acledaaib.onmicrosoft.com>
2025-07-30 23:05:32 +0700

    add bottom navbar skeleton no fragment yet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,8 +57,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/elts/HomeSCreenActivity.kt
+++ app/src/main/java/com/elts/HomeSCreenActivity.kt
@@ -1,20 +1,50 @@
 package com.elts
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.elts.databinding.ActivityHomeScreenBinding
+
 
 class HomeSCreenActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomeScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home_screen)
+        binding = ActivityHomeScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_explore -> {
+                    // Handle Home tab click
+                    // e.g., switch fragment or start activity
+                    Toast.makeText(this, "Explore", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.menu_prepare -> {
+                    Toast.makeText(this, "Prepare", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.menu_results -> {
+                    Toast.makeText(this, "Results", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+
     }
 }
